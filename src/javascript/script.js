683d199e88169bafb6962bdc43d1767211421b92
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-02 20:30:33 -0300

    feat
    Selecionando variáveis via DOM.

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,9 +1,9 @@
 // Seleção de elementos
 const todoForm = document.querySelector("#todo-form");
-const todoForm = document.querySelector("#todo-form");
-const todoForm = document.querySelector("#todo-form");
-const todoForm = document.querySelector("#todo-form");
-const todoForm = document.querySelector("#todo-form");
+const todoinput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // Funções
 
