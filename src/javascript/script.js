8d924c08e98be18c728aadf2b6a73899e4e58f9f
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-02 20:36:30 -0300

    feat
    Adicionando função para prevenir evento do formulário e capturar o input.

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,11 +1,21 @@
 // Seleção de elementos
 const todoForm = document.querySelector("#todo-form");
-const todoinput = document.querySelector("#todo-input");
+const todoInput = document.querySelector("#todo-input");
 const todoList = document.querySelector("#todo-list");
 const editForm = document.querySelector("#edit-form");
 const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
 // Funções
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const inputValue = todoInput.value
+
+    if (inputValue) {
+        console.log(inputValue);
+    }
+})
+
 
 
 //Eventos
