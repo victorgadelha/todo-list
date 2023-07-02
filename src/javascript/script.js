435e78b3ce466756c9bd0558d897d4cef913c2a1
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-02 20:28:56 -0300

    feat
    Adicionando variáveis no JS.

---
+++ src/javascript/script.js
@@ -0,0 +1,11 @@
+// Seleção de elementos
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+const todoForm = document.querySelector("#todo-form");
+
+// Funções
+
+
+//Eventos
