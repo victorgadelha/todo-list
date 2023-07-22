045e149e8d2653bc176ed016f21ff13fb975fded
João Victor Gadelha <108595157+victorgadelha@users.noreply.github.com>
2023-07-22 18:27:20 -0300

    feat: adicionando função de editar tarefa

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -46,6 +46,18 @@ const saveTodo = (text) => {
         todoList.classList.toggle("hide")
     }
 
+    const updateTodo = (text) => {
+        const todos = document.querySelectorAll(".todo")
+
+        todos.forEach((todo) => {
+            let todoTitle = todo.querySelector("h3")
+
+            if(todoTitle.innerText === oldInputValue) {
+                todoTitle.innerText = text
+            }
+        })
+    }
+
 //Eventos
 
 todoForm.addEventListener("submit", (e) => {
@@ -61,7 +73,7 @@ todoForm.addEventListener("submit", (e) => {
 document.addEventListener("click", (e) => {
     const targetEl = e.target
     const parentEl = targetEl.closest("div")
-    let todoTitle
+    let todoTitle;
 
     if (parentEl && parentEl.querySelector("h3")) {
         todoTitle = parentEl.querySelector("h3").innerText
@@ -76,10 +88,21 @@ document.addEventListener("click", (e) => {
     if (targetEl.classList.contains("edit-todo")) {
         toggleForms()
         editInput.value = todoTitle
-        oldInput.value = todoTitle
+        oldInputValue = todoTitle
     }   
 })
 cancelEditBtn.addEventListener("click", (e) => {
     e.preventDefault()
+    toggleForms()
+})
+
+editForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+    const editInputValue = editInput.value
+
+    if(editInputValue) {
+        updateTodo(editInputValue)
+    }
+
     toggleForms()
 })
